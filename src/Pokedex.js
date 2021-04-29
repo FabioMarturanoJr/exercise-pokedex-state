e8f877568007549de3ebe4f8f1073be12f33d836
Fabio Marturano <fabiocmjor@gmail.com>
2021-04-29 20:43:50 -0300

    requisito 2

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// import pokemons from './data';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
@@ -6,9 +7,10 @@ class Pokedex extends React.Component {
   constructor(props) {
     super(props);
     this.switchPokemon = this.switchPokemon.bind(this);
+    this.switchFilter = this.switchFilter.bind(this);
 
     this.state = {
-      pokemon: props.pokemons[0],
+      pokemon: props.pokemons[1],
       key: 0,
       filter: 'Fire',
     };
@@ -16,19 +18,25 @@ class Pokedex extends React.Component {
 
   switchPokemon() {
     const { pokemons } = this.props;
-    const { key } = this.state;
+    const { key, filter } = this.state;
+    const pkfiltreds = pokemons.filter((poke) => poke.type === filter);
 
-    if (key > 7) {
+    if (key > pkfiltreds.length - 2) {
       this.setState({
-        pokemon: pokemons[0],
+        pokemon: pkfiltreds[0],
         key: 0,
       });
     } else {
       this.setState({
-        pokemon: pokemons[key + 1],
+        pokemon: pkfiltreds[key + 1],
         key: key + 1,
       });
-    }    
+    }
+  }
+
+  async switchFilter({ target : { id } }) {
+    await this.setState({ filter : id });
+    this.switchPokemon();
   }
 
   render() {        
@@ -37,10 +45,13 @@ class Pokedex extends React.Component {
         <div className="pokedex">
           <Pokemon key={this.state.pokemon.id} pokemon={this.state.pokemon} />
         </div>
-        <button onClick={this.switchPokemon}>Switch</button>
+        <button onClick={this.switchPokemon}>All</button>
         <div>
-          <button className="filterButton">Fire</button>
-          <button className="filterButton">Psychic</button>
+          <button onClick={this.switchFilter} className="filterFire" id="Fire">Fire</button>
+          <button onClick={this.switchFilter} className="filterPsychic" id="Psychic">Psychic</button>
+          <button onClick={this.switchFilter} className="filterElectric" id="Electric">Electric</button>
+          <button onClick={this.switchFilter} className="filterBug" id="Bug">Bug</button>
+          <button onClick={this.switchFilter} className="filterPoison" id="Poison">Poison</button>
         </div>
         <p>
           Filtro: {this.state.filter}
